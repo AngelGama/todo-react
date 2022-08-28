f4286f4b100eeab981d5f09c10a2a2a1d3a38cde
Angel Gama <angelgamaxd@gmail.com>
2022-08-27 20:46:34 -0500

    Events added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,30 @@ import React from "react";
 import './TodoItem.css'
 
 function TodoItem(props) {
+
+    const onComplete = () => {
+        alert('Completaste: ' + props.text);
+    };
+
+    const onDelete = () => {
+        alert('Eliminaste: ' + props.text);
+    };
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+            <span
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
                 ✓
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
                 ✕
             </span>
         </li>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,17 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch() {
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
+
     return (
-        <input className="TodoSearch" placeholder="¿Qué tienes en mente?" />
+        <input
+            className="TodoSearch"
+            placeholder="¿Qué tienes en mente?"
+            onChange={onSearchValueChange}
+        />
     );
 }
 
